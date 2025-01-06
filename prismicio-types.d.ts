db507f47d63767521c1dba845018c4fe71f42d8e
victor Macharia <Techvictor653@gmail.com>
2025-01-06 23:34:46 +0300

    feat: finished the testimonial slice

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -4,7 +4,10 @@ import type * as prismic from "@prismicio/client";
 
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
 
-type HomepageDocumentDataSlicesSlice = HeroSlice;
+type HomepageDocumentDataSlicesSlice =
+  | TestimonialsSlice
+  | FeaturesSlice
+  | HeroSlice;
 
 /**
  * Content for Homepage documents
